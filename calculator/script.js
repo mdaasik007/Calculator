a07fe521691643f5a9fbd388a653c115c7c73748
Mohammedaasik <aasikflipper@gmail.com>
2025-08-30 07:53:16 +0500

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,21 @@
+   
+        let outputScreen = document.getElementById("output-screen");
+
+        function display(num) {
+            outputScreen.value += num;
+            console.log(outputScreen);
+         }
+         function Calculate() {
+    try {
+        outputScreen.value = eval(outputScreen.value);
+    } catch (err) {
+        alert("Invalid");
+    }
+}
+         function clear() {
+    outputScreen.value = "";
+}
+
+         function del() {
+    outputScreen.value = outputScreen.value.slice(0, -1);
+}
